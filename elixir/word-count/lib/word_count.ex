e481f57de7e12bdbd1e98d58e9ab5cd9a953b8d7
Franciele Portugal <franciele.santos@stone.com.br>
2021-03-24 18:50:21 -0300

    solve exercise word count

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,5 +6,19 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+
+    normalize(sentence)
+    |> String.split()
+    |> Enum.frequencies()
+  end
+
+  @doc """
+  Normalize a setence.
+  """
+  @spec count(String.t()) :: String.t()
+  defp normalize(sentence) do
+    sentenceNormalized = Regex.replace(~r"(*UTF)[^\w\s\-]", sentence, "")
+    sentenceNormalized = Regex.replace(~r/_/, sentenceNormalized, " ")
+    String.downcase(sentenceNormalized)
   end
 end
